d32500f0061154847825c69d459a2d560314c251
Alec Shiya <alec.shiya@pushpay.com>
2018-04-24 16:45:27 +1200

    Fiddling with script

--- build.cmd
+++ build.cmd
@@ -1,7 +1,7 @@
-cd ./hello-world-repo
+cd hello-world-repo
 dotnet restore
 dotnet build
-dotnet test ./aws-csharp-tests/
-cd ./aws-csharp
+dotnet test aws-csharp-tests
+cd aws-csharp
 dotnet lambda package --configuration release --framework netcoreapp2.0 --output-package bin/release/netcoreapp2.0/deploy-package.zip
 cd ..
