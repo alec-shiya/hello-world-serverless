f8dd2e31f74d92ede61f635a5e87e8c02b32eb2e
Alec Shiya <alec.shiya@pushpay.com>
2018-04-24 16:23:18 +1200

    Updating build scripts

---
+++ build.cmd
@@ -0,0 +1,6 @@
+dotnet restore
+dotnet build
+dotnet test ./aws-csharp-tests/
+cd ./aws-csharp
+dotnet lambda package --configuration release --framework netcoreapp2.0 --output-package bin/release/netcoreapp2.0/deploy-package.zip
+cd ..
