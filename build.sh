12c27f11de59ddebc00cc9cb30d48c7f7085a410
Alec Shiya <alec.shiya@pushpay.com>
2018-04-24 16:53:02 +1200

    build config

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
+pwd
 dotnet restore
 dotnet build
 dotnet test ./aws-csharp-tests/
 cd ./aws-csharp
+pwd
 dotnet lambda package --configuration release --framework netcoreapp2.0 --output-package bin/release/netcoreapp2.0/deploy-package.zip
 cd ..
+pwd
